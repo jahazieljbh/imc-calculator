faad930e66d76ce5ee1d797aaa030dea511b4317
Jahaziel Basilio Hernandez <62615784+jahazieljbh@users.noreply.github.com>
2021-01-04 20:42:04 -0600

    Revert "Signup to Ui-imc"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,18 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UiImcComponent } from './ui-imc/ui-imc.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './login/login.component';
-import { HttpClientModule } from '@angular/common/http';
-import { StorageService } from "../services/storage.service";
-import { SignupComponent } from './signup/signup.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UiImcComponent,
-    LoginComponent,
-    SignupComponent,
-    HeaderComponent,
-    HomeComponent
+    UiImcComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [StorageService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
